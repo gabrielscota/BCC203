93084b11bf170d89084b7ece2405f1ec039a7164
Gabriel Scotá Arruda <gabrielscota2015@gmail.com>
2020-09-30 23:35:35 -0300

    Colocando o ifndef para evitar rededeclaração

--- structs.c
+++ structs.c
@@ -1,6 +1,9 @@
 #include <stdlib.h>
 #include <string.h>
 
+#ifndef STRUCTS_H
+#define STRUCtS_H
+
 #define FF 40 // Numero de fitas magneticas
 #define F (FF/2)
 
@@ -12,19 +15,4 @@ typedef struct Registro {
     char curso[31];
 } Registro;
 
-void criarArquivoTemporario(FILE *arq, FILE *temp, int n)
-{
-    Registro registro;
-    for (int i = 0; i < n; i++)
-    {
-        // // Fazendo a leitura do numero de inscrição, nota e um ' ' no final para começar a ler o proximo dado
-        fscanf(arq, "%ld %lf ", &registro.numeroInscricao, &registro.nota);
-        fgets(registro.estado, 3, arq);
-        fseek(arq, 1, 1);
-        fgets(registro.cidade, 50, arq);
-        fseek(arq, 2, 1);
-        fgets(registro.curso, 30, arq);
-        // printf("%ld | %lf | %s | %s | %s\n", registro.numeroInscricao, registro.nota, registro.estado, registro.cidade, registro.curso);
-        fwrite(&registro, sizeof(Registro), 1, temp);
-    }
-}
+#endif
